88956f9f735f8f8f2f293540916db8911242dbae
Perajti Pasupat <perajit.p@gmail.com>
2025-02-26 22:49:18 +0700

    feat: fix timezone issue for default theme calculation

--- app/_utils/theme.util.ts
+++ app/_utils/theme.util.ts
@@ -1,9 +1,11 @@
 import { Theme } from '../_types/theme.type';
 
-const dayCutOffHours = 6;
-const nightCutoffHours = 18;
+const dayCutoffThHours = 6;
+const nightCutoffThHours = 18;
 
 export const getDefaultTheme = (): Theme => {
-  const currentHours = new Date().getHours();
-  return currentHours >= dayCutOffHours && currentHours < nightCutoffHours ? 'day' : 'night';
+  const currentUtcHours = new Date().getUTCHours();
+  const currentThHours = currentUtcHours + 7;
+
+  return currentThHours >= dayCutoffThHours && currentThHours < nightCutoffThHours ? 'day' : 'night';
 };
